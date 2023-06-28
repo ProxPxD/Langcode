7f8897e3c68969e6360dca9ae9028cede9c09729
ProxPxD <piotr10tutek@poczta.onet.pl>
2023-06-28 20:29:26 +0200

    create first morpheme tests

---
+++ tests/MorphologicalProcessApplicationTests/application.py
@@ -0,0 +1,72 @@
+import parameterized as parameterized
+
+from langcode import Morpheme
+from tests.abstractTest import AbstractTest
+from tests.testutils import remove_alnum
+
+
+class ApplicationTest(AbstractTest):
+	@classmethod
+	def _get_test_name(cls) -> str:
+		return 'Application'
+
+	is_generated = False
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		if not self.is_generated:
+			self.gen_all_test_morpheme_processes()
+			self.is_generated = True
+
+	morpheme_change_parameters = [
+		('prefix',             					Morpheme, {'over+':    				  [('cook', 'overcook'), ('look', 'overlook')]}),
+		('postfix',   	   	   					Morpheme, {'+ful':     				  [('wonder', 'wonderful'), ('stress', 'stressful')]}),
+		('optional_postfix',   					Morpheme, {'-e^n+t':            	  [('üben', 'übt'), ('sammeln', 'sammelt')]}),
+		('cond_postfix',   	        			Morpheme, {'(-a|-o?-),(-a?+y:+a)':    [('kot', 'kota'), ('mama', 'mamy'), ('okno', 'okna')]}),
+		('autogrouping_infix',      			Morpheme, {r'-ek+usz\1':  	          [('kwiatek', 'kwiatuszek')],
+								 	           			   r'u-\1r+':    		      [('umat', 'urmat'), ('ukat', 'urkat')]}),
+		('context_grouping_jointly_infix', 	    Morpheme, {r'V1C*1-~V1-V1+r+': 		  [('umat', 'urmat'), ('amat', 'armat')]}),
+		('context_grouping_separately_infix', 	Morpheme, {r'V1C*1-~V1-,r+,V1+': 	  [('umat', 'urmat'), ('amat', 'armat')]}),
+		('circumfix',      	 					Morpheme, {'ge+t':     				  [('sag', 'gesagt'), ]}),
+		('interfix',         					Morpheme, {'+o+':      				  [('myśl', 'zbrodnia', 'myślozbrodnia'),
+											  	  				   	  				   ('kot', 'schron', 'kotoschron'),
+											      				       				   ('kot', 'pies', 'kotopies'),
+																	   				   ('marsz', 'bieg', 'marszobieg')]}),
+		('umlaut_simulfix', 		 			Morpheme, {'-V1C1~V1:u>ü|o>ö|a>ä':    [('groß', 'größer')]}),
+		#('syllable_simulfix', 		 			Morpheme, {'-V1C1~V1:u>ü|o>ö|a>ä':    [('groß', 'größer')]}),
+		('mutation_simulfix', 		 			Morpheme, {'-C1:k>cz,+ek': [('paweł', 'pawełek'), ('pawełek', 'pawełeczek')],
+														   '-C1~C1:k>cz,+ek':    [('paweł', 'pawełek'), ('pawełek', 'pawełeczek')]}),
+	]
+
+	@classmethod
+	def gen_all_test_morpheme_processes(cls):
+		param_num = 3
+		adjust_params = lambda params: (list(params) + [None]*param_num)[:param_num]
+		for (op_name, morpheme_class, forms_pairs) in map(adjust_params, cls.morpheme_change_parameters):
+			for form, to_apply_tos_expecteds in forms_pairs.items():
+				for to_apply_tos_expected in to_apply_tos_expecteds:
+					to_apply_tos = to_apply_tos_expected[:-1]
+					expected = to_apply_tos_expecteds[-1]
+					to_apply_to_str = '_and_'.join(to_apply_tos)
+					test_name = f'test_apply_{op_name}_on_{to_apply_to_str}_to_get_{expected}_using_{morpheme_class.__name__.lower()}'
+					print(test_name)
+					test = cls.gen_test_morpheme_process(morpheme_class, form, to_apply_tos, expected)
+					setattr(ApplicationTest, test_name, test)
+
+	@classmethod
+	def gen_test_morpheme_process(cls, morpheme_class: type[Morpheme], form: str, to_apply_tos: list[str], expected: str):
+		def test_morpheme_process(self, *args):
+			morpheme = morpheme_class(form)
+			actual = morpheme(*to_apply_tos)
+			self.assertEqual(expected, actual)
+		return test_morpheme_process
+
+	# #  name_func=lambda method, param_num, param: f'{method.__name__}_{param_num}_apply_{param.args[0]}_with_{param.args[2].__name__}')
+	# @parameterized.expand([
+	# 	('add_prefix_with_morpheme', Morpheme, 'over+', ['cook'], ['overcook']),
+	# 	('', '', Morpheme),
+	# 	('', '', Morpheme),
+	# 	('', '', Morpheme),
+	# ])
+	# def apply_form_test(self, name, morpheme, form, to_applies, expecteds):
+	# 	pass
---
+++ tests/all_tests.py
@@ -0,0 +1,38 @@
+import unittest
+from typing import Iterable
+
+from tests.Grammar.grammar_test import GrammarTest
+from tests.MorphologicalProcessApplicationTests.application import ApplicationTest
+from tests.abstractTest import AbstractTest
+
+all_tests = [
+    #ApplicationTest,
+    GrammarTest,
+]
+
+
+def get_tests_from_dir(dir_name: str = None):
+    if not dir_name:
+        return all_tests
+    raise NotImplementedError
+
+
+def run_tests(to_runs: Iterable):
+    failure, errors, total, skipped = 0, 0, 0, 0
+    for test_class in to_runs:
+        test = test_class()
+        unittest.main(module=test, exit=False)
+
+        failure += test.failure
+        errors += test.errors
+        total += test.total
+        skipped += test.skipped
+
+    print()
+    print('#' * (2 * AbstractTest.half_sep_length))
+    print('Total test statistics:')
+    AbstractTest.print_statistics(failure, errors, skipped, total)
+
+
+if __name__ == '__main__':
+    run_tests(get_tests_from_dir())
