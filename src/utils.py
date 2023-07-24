e512e198d2e6236a08692dd37d4bfc1239de8054
ProxPxD <piotr10tutek@poczta.onet.pl>
2023-07-24 22:09:28 +0200

    addd dict class to utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
-from typing import Iterable
+from dataclasses import asdict
+from typing import Iterable, Callable, Any
 
 
 def get_name(instance):
@@ -17,3 +18,21 @@ def reapply(fn, arg, n=None, until=None, as_long=None):
 
 
 to_last_list = lambda elem: reapply(lambda c: c[0], elem, as_long=lambda c: isinstance(c, list) and len(c) == 1 and isinstance(c[0], list))
+
+
+class DictClass:
+    def __getitem__(self, item):
+        return self.__dict__[item]
+
+    def __setitem__(self, key, value):
+        print('To verify: ',  self.__dict__)
+        return self.__dict__.__setitem__(key, value)
+
+    dict: Callable[[], dict] = asdict
+    values: Callable[[], Any] = lambda self: self.dict().values()
+    keys: Callable[[], Any] = lambda self: self.dict().keys()
+    items: Callable[[], Any] = lambda self: self.dict().items()
+
+    @classmethod
+    def map_to_contained_key(cls, k) -> str | None:
+        return next(filter(k.__contains__, cls().keys()), None)
