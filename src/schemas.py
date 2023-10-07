941f5b295129685c6602d9a0fe5704a0048a40a9
ProxPxD <piotr10tutek@poczta.onet.pl>
2023-10-07 14:03:21 +0200

    fix validation

--- src/schemas.py
+++ src/schemas.py
@@ -2,29 +2,61 @@ from types import NoneType
 from typing import Callable, Any
 
 from src.constants import LangData
+from src.exceptions import InvalidYamlException, Messages
 
 
 class SchemaValidator:
     @classmethod
     def validate(cls, to_val: dict, kind: str = LangData.LANGUAGE) -> bool:
-        return cls.get_validation(kind)(to_val)
+        return cls.get_validation(kind)(kind, to_val)
 
     @classmethod
-    def validate_dict(cls, to_val: dict, requireds=None, **kwargs) -> bool:
+    def validate_dict(cls, curr_kind: str, to_val: dict, requireds=None, next_kind: str = None, **kwargs) -> bool:
         if isinstance(to_val, (str, NoneType)):
             return True
-        requireds = requireds or tuple()
-        has_requireds = all((required in to_val for required in requireds))
-        return has_requireds and all((isinstance(name, str) and cls.get_validation(name)(elem, **kwargs) for name, elem in to_val.items()))
+        cls._validate_dict_required(curr_kind, to_val, requireds)
+        return cls._validate_dict_subfields(curr_kind, to_val, next_kind, **kwargs)
+
+    @classmethod
+    def _validate_dict_required(cls, curr_kind: str, to_val: dict, requireds: tuple):
+        if requireds:
+            for req_to_val in requireds:
+                if req_to_val not in to_val:
+                    raise InvalidYamlException(Messages.LACK_OF_REQUIRED_FIELD % (curr_kind, req_to_val))
+        return True
+
+    @classmethod
+    def _validate_dict_subfields(cls, curr_kind: str, to_val: dict, next_kind: str = None, **kwargs) -> bool:
+        get_next_val = lambda name: cls.get_validation(next_kind) if next_kind else cls.get_validation(name)
+        for name, elem in to_val.items():
+            try:
+                get_next_val(name)(name, elem, **kwargs)
+            except InvalidYamlException as iye:
+                raise InvalidYamlException(f'{curr_kind} > ', *iye.args)
+        return True
 
     @classmethod
     def get_validation(cls, kind: str) -> Callable[[dict | str, Any, ...], bool]:
-        try:
-            func = getattr(cls, f'validate_{kind}')
-        except Exception:
-            func = cls.validate_dict
-        return func
+        return getattr(cls, f'validate_{kind}', cls.validate_dict)
 
     @classmethod
-    def validate_language(cls, to_val: dict, **kwargs) -> bool:
-        return cls.validate_dict(to_val, requireds=(LangData.FEATURES, LangData.MORPHEMES, LangData.GRAPHEMES), **kwargs)
+    def validate_language(cls, curr_kind: str, to_val: dict, **kwargs) -> bool:
+        return cls.validate_dict(curr_kind, to_val, requireds=(LangData.MORPHEMES, ), **kwargs)
+
+    @classmethod
+    def validate_morphemes(cls, curr_kind, to_val, **kwargs) -> bool:
+        return cls.validate_dict(curr_kind, to_val, next_kind='morpheme', **kwargs)
+
+    @classmethod
+    def validate_morpheme(cls, curr_kind, to_val, **kwargs) -> bool:
+        if to_val is None:
+            raise InvalidYamlException(Messages.NOT_DEFINED % curr_kind)
+        exclusives_with_form = (LangData.COMPOUND, )
+        is_any_exclusives_with_form = any(map(to_val.__contains__, exclusives_with_form))
+        is_form_in = LangData.FORM in to_val
+
+        if is_form_in and is_any_exclusives_with_form:
+            raise InvalidYamlException(Messages.FORMING_KEYS_TOGETHER)
+        if not is_form_in and not is_any_exclusives_with_form:
+            raise InvalidYamlException(Messages.NO_FORMING_KEY)
+        return True
