3628dad2faf54093294404cbf0e8b18e4e3f1dc6
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-02-18 13:02:52 +0100

    refactor and extend dot dict

--- src/dot_dict.py
+++ src/dot_dict.py
@@ -1,19 +1,26 @@
 from __future__ import annotations
 
-from typing import Iterable, Sequence, Optional, Any
+from typing import Iterable, Sequence, Optional, Any, Never
 
 from src.constants import yaml_type
 from src.utils import to_list
 
 
+class DotDictNone(object):
+    def __bool__(self):
+        return False
+
+
 class DotDict:
-    def __init__(self, d: Any, *, defaults: Optional[dict] = None):
+    def __init__(self, d: Any = DotDictNone(), *, defaults: Optional[dict] = DotDictNone(), default_val: Any = DotDictNone()):
         self._curr: yaml_type | Any = d() if isinstance(d, DotDict) else d
         self._defaults = defaults
+        self._default_val: Any = default_val
 
-    def _get_default(self, item: str, curr_defaults: Optional[dict] = None) -> Any:
+    def _get_default(self, item: str, curr_defaults: Optional[dict] = DotDictNone()) -> Any:
         curr_defaults = curr_defaults or self._defaults
-        return curr_defaults[item] if curr_defaults is not None else None
+        default = curr_defaults[item] if hasattr(curr_defaults, '__getitem__') else self._default_val
+        return default
 
     def __getitem__(self, path: str | Sequence[str]) -> DotDict:
         path = to_list(path)
@@ -21,7 +28,7 @@ class DotDict:
         while path:
             item = path.pop(0)
             curr_defaults = self._get_default(item, curr_defaults)
-            curr_value = curr_value.get(item, curr_defaults) if isinstance(curr_value, dict) else None
+            curr_value = curr_value.get(item, curr_defaults) if isinstance(curr_value, dict) else self._default_val
         return DotDict(curr_value, defaults=curr_defaults)
 
     def __getattr__(self, path: str | Sequence[str]) -> DotDict:
@@ -37,7 +44,7 @@ class DotDict:
         curr = self.get()
         if not isinstance(curr, dict):
             return bool(curr)
-        sub_dot_dicts = (DotDict(val, defaults=self._get_default(key)) for key, val in curr.items())
+        sub_dot_dicts = (DotDict(val, defaults=self._get_default(key), default_val=self._default_val) for key, val in curr.items())
         return any(map(bool, sub_dot_dicts))
 
     def __contains__(self, path: str | Sequence[str]):
@@ -50,17 +57,23 @@ class DotDict:
             curr_value = curr_value.get(item)
         return is_present
 
-    def keys(self) -> Iterable[str]:
-        yield from self._curr.keys() if isinstance(self._curr, dict) else (None, )
+    def keys(self) -> Iterable[str | None]:
+        match self._curr:
+            case dict(): return self._curr.keys()
+            case DotDictNone(): return iter(())
+            case _: return (None,)
 
     def values(self):
-        if isinstance(self._curr, dict):
-            yield from (DotDict(val, defaults=self._get_default(key)) for key, val in self._curr.items())
-        else:
-            yield DotDict(self._curr)
+        match self._curr:
+            case dict(): return (DotDict(val, defaults=self._get_default(key)) for key, val in self._curr.items())
+            case DotDictNone(): return iter(())
+            case _: (DotDict(self._curr), )
 
     def items(self):
         yield from zip(self.keys(), self.values())
 
     def __str__(self):
+        return f'{self.__class__.__name__}({self._curr})'
+
+    def __repr__(self):
         return f'{self.__class__.__name__}({self._curr})'
