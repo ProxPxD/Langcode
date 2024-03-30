55a797b14e71376d43108e8c695621d2966540ca
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-03-30 23:11:20 +0100

    implement schemas (but rules) in pydantic

--- src/schema_validator.py
+++ src/schema_validator.py
@@ -1,111 +1,116 @@
-from types import NoneType
+from __future__ import annotations
 
+from abc import ABC
+from itertools import starmap
+from typing import Optional, List, AnyStr, Iterable, Dict, Tuple
+
+from more_itertools import side_effect, consume
+from pydantic import BaseModel, field_validator, model_validator
+from toolz import curry
+
+import utils
 from src.constants import SimpleTerms
-from src.exceptions import InvalidYamlException, Messages
-from src.language_components import Language
-
-from strictyaml import load, Map, Str, Int, Any, Seq, YAMLError, MapPattern, MapCombined, Optional, Bool
+from src.lang_typing import Config, Kind, Resolution, UnitConf
+from src.language_components import Unit, Feature
 
 # TODO: Decision: do the I and potentially extend for the III one later and maybe with a flag
 # see: https://github.com/ProxPxD/Langcode/issues/7
 
-general_schema = Map({
-
-})
-
-features_schema = Map({
-  ## nested recurrence as in deutsch features, but the features can have subfeatures
-})
-
-morpheme_schema = MapCombined(
-    {
-        Optional('bound'): Bool()
-    },
-    Str(),
-    Any()
-)
-
-graphemes_schema = Map({
-
-})
-
-morphemes_schema = MapPattern(Str(), morpheme_schema)
+ID = AnyStr
 
 
-rules_schema = Map({
+@curry
+def config_to_unit(kind: str, name: str, config: Config):
+    return Unit(name=name, kind=kind, features=config)
 
-})
+
+class GeneralSchema(BaseModel):
+    pass
 
 
-lang_schema = Map({
-    'general': general_schema,
-    'features': features_schema,  # TODO: think if should be inside graphemes and morphemes.yaml
-    'graphemes': graphemes_schema,
-    'morphemes': morphemes_schema,
-    'rules': rules_schema,
-})
+class MorphemeSpecificationSchema(BaseModel):
+    pass
 
-#
-# class SchemaValidator:
-#     @classmethod
-#     def validate(cls, to_val: dict, kind: str = LangData.LANGUAGE) -> bool:
-#         return cls.get_validation(kind)(kind, to_val)
-#
-#     @classmethod
-#     def validate_dict(cls, curr_kind: str, to_val: dict, requireds=None, next_kind: str = None, **kwargs) -> bool:
-#         if isinstance(to_val, (str, NoneType)):
-#             return True
-#         cls._validate_dict_required(curr_kind, to_val, requireds)
-#         return cls._validate_dict_subfields(curr_kind, to_val, next_kind, **kwargs)
-#
-#     @classmethod
-#     def _validate_dict_required(cls, curr_kind: str, to_val: dict, requireds: tuple):
-#         if requireds:
-#             for req_to_val in requireds:
-#                 if req_to_val not in to_val:
-#                     raise InvalidYamlException(Messages.LACK_OF_REQUIRED_FIELD % (curr_kind, req_to_val))
-#         return True
-#
-#     @classmethod
-#     def _validate_dict_subfields(cls, curr_kind: str, to_val: dict, next_kind: str = None, **kwargs) -> bool:
-#         get_next_val = lambda name: cls.get_validation(next_kind) if next_kind else cls.get_validation(name)
-#         for name, elem in to_val.items():
-#             try:
-#                 get_next_val(name)(name, elem, **kwargs)
-#             except InvalidYamlException as iye:
-#                 raise InvalidYamlException(f'{curr_kind} > ', *iye.args)
-#         return True
-#
-#     @classmethod
-#     def get_validation(cls, kind: str) -> Callable[[dict | str, Any, ...], bool]:
-#         return getattr(cls, f'validate_{kind}', cls.validate_dict)
-#
-#     @classmethod
-#     def validate_language(cls, curr_kind: str, to_val: dict, **kwargs) -> bool:
-#         return cls.validate_dict(curr_kind, to_val, requireds=(LangData.MORPHEMES, ), **kwargs)
-#
-#     @classmethod
-#     def validate_morphemes(cls, curr_kind, to_val, **kwargs) -> bool:
-#         return cls.validate_dict(curr_kind, to_val, next_kind=LangData.MORPHEMES, **kwargs)
-#
-#     @classmethod
-#     def validate_morpheme(cls, curr_kind, to_val, **kwargs) -> bool:
-#         if to_val is None:
-#             raise InvalidYamlException(Messages.NOT_DEFINED % curr_kind)
-#         exclusives_with_form = (LangData.COMPOUND, )
-#         is_any_exclusives_with_form = any(map(to_val.__contains__, exclusives_with_form))
-#         is_form_in = LangData.FORM in to_val
-#
-#         if is_form_in and is_any_exclusives_with_form:
-#             raise InvalidYamlException(Messages.FORMING_KEYS_TOGETHER)
-#         if not is_form_in and not is_any_exclusives_with_form:
-#             raise InvalidYamlException(Messages.NO_FORMING_KEY)
-#         return True
-#
-#     @classmethod
-#     def validate_rules(cls, curr_kind, to_val, **kwargs) -> bool:
-#         return cls.validate_dict(curr_kind, to_val, next_kind=LangData.RULES, **kwargs)
-#
-#     @classmethod
-#     def validate_rule(cls, curr_kind, to_val, **kwargs) -> bool:
-#         return False
+
+class MorphemesFeaturesSchema(BaseModel):
+    pass
+
+
+class UnitSchema(BaseModel, ABC):
+    @classmethod
+    def map_unit_conf_to_units(cls, elems: UnitConf, kind: Kind) -> Iterable[Unit]:
+        normalized = utils.map_conf_list_to_dict(elems)
+        unit_elems = dict(starmap(cls.create_unit(kind), normalized.items()))  # TODO: think if initial structure of morpheme config is not required such as checking if features exist
+        return unit_elems
+
+    @classmethod
+    @curry
+    def create_unit(cls, kind: Kind, name: str, conf: dict) -> Unit:
+        # TODO: Connect to features
+        unit = Unit(name=name, kind=kind)
+        unit.load_conf(conf)
+        return unit
+
+
+class MorphemesSchema(UnitSchema):
+    elems: Optional[UnitConf]
+    features: Optional[MorphemesFeaturesSchema]
+
+    @field_validator('elems')  # TODO: think of validating it deeper
+    def val_elems(self, elems) -> Iterable[Unit]:
+        return self.map_unit_conf_to_units(elems, SimpleTerms.MORPHEME)
+
+
+class RulesSchema(BaseModel):
+    pass
+
+
+class FeatureSchema(BaseModel):
+    type: Optional[Resolution]
+    elems: List[FeatureSchema | AnyStr] | Dict[AnyStr, FeatureSchema] | Dict[AnyStr, AnyStr]  # TODO: name such type(s)
+
+    @field_validator('elems')
+    def create_features(self, elems) -> List[Tuple[Feature, Dict]]:
+        normalized = utils.map_conf_list_to_dict(elems)
+        features = [(Feature(name=name, kind=None), conf) for name, conf in normalized.items()]
+        return features
+
+    @model_validator(mode='after')
+    def set_children_and_type(self, values):
+        features: List[Tuple[Feature, Dict]] = values.get(SimpleTerms.ELEMS)
+        for feature, conf in features:
+            if elems := conf.get(SimpleTerms.ELEMS):
+                children, children_conf = tuple(zip(*elems))
+                default_feature_type = SimpleTerms.DISJOINT if utils.is_all_dict_of_none(children_conf) else SimpleTerms.JOINT
+                feature.type = conf.get(SimpleTerms.TYPE, default_feature_type)
+                consume(side_effect(feature.children.manager.connect, children))
+        return values
+
+
+class FeaturesSchema(BaseModel):
+    graphemes: FeatureSchema
+    morphemes: FeatureSchema
+
+    @classmethod
+    def set_kind(cls, phemes: list[Feature], kind: Kind) -> List[Feature]:
+        utils.apply_to_tree(
+            phemes,
+            lambda curr: setattr(curr, 'kind', kind),
+            lambda curr: curr.children,
+        )
+        return phemes
+
+    @field_validator('graphemes')
+    def val_graphemes(self, graphemes):
+        return self.set_kind(graphemes, SimpleTerms.GRAPHEME)
+
+    @field_validator('morphemes')
+    def val_morphemes(self, morphemes):
+        return self.set_kind(morphemes, SimpleTerms.MORPHEME)
+
+
+class LanguageSchema(BaseModel):  # TODO: think if morphemes shouldn't be required
+    general: Optional[GeneralSchema]
+    morphemes: Optional[MorphemesSchema]
+    rules: Optional[RulesSchema]
+    features: Optional[FeaturesSchema]
