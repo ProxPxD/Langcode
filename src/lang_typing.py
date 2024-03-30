7ac9b9d3938775e0e5199a956ee97be55b2c3ac5
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-03-30 23:06:08 +0100

    create typing

---
+++ src/lang_typing.py
@@ -0,0 +1,12 @@
+from typing import Dict, Any, Literal, AnyStr, List
+
+from src.constants import SimpleTerms
+
+BasicYamlType = str | int | bool | None
+ComplexYamlType = dict | list
+YamlType = BasicYamlType | ComplexYamlType
+
+Config = Dict[str, Any]  # TODO: Check recurrent hinting  # TODO: verify name and/or make another
+Kind = Literal[SimpleTerms.MORPHEME, SimpleTerms.GRAPHEME, None]
+Resolution = Literal[SimpleTerms.JOINT, SimpleTerms.DISJOINT]
+UnitConf = Dict[AnyStr, Any] | List[AnyStr | Any]
