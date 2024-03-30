6a29790e9e2aa0f6a84a64df23e28879ce6f26c4
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-03-30 23:06:40 +0100

    add useful funcs

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,19 @@
-from typing import Iterable, Callable, Any, Collection, Tuple
+import operator as op
+from typing import Iterable, Callable, Any, List, AnyStr, Dict, Type, TypeVar
 
 from toolz.curried import *
 
-from src.constants import basic_yaml_type
-import operator as op
+from src.lang_typing import BasicYamlType
 
 fjoin = compose = pipeline = compose_left
 eq = curry(op.eq)
+T = TypeVar('T')
+
+is_ = flip(isinstance)
+is_dict = is_(dict)
+is_list = is_(list)
+is_int = is_(int)
+is_str = is_(str)
 
 
 def get_name(instance):
@@ -46,54 +53,61 @@ class DictClass:
         return cls.dict().items()
 
 
-def word_to_basics(word: str, basics: Collection[str], skip_missing=False, yield_index=False, start_from_one=False) -> Collection[str | Tuple[str, int] | None]:
-    basics = sorted(basics, reverse=True)
-    index = 1 if start_from_one else 0
-    missing = ''
-    while word:
-        first = word[0]
-        starting = next(filter(lambda b: b.startswith(first), basics), None)
-        if starting is None:
-            missing += first
-            word = word[1:]
-        else:
-            if missing:
-                if not skip_missing:
-                    if yield_index:
-                        yield missing, index
-                    else:
-                        yield missing
-                index += len(missing)
-                missing = ''
-            if yield_index:
-                yield starting, index
-                index += len(starting)
-            else:
-                yield starting
-            word = word[len(starting):]
-
-    if missing and not skip_missing:
-        if yield_index:
-            yield missing, index
-        else:
-            yield missing
-
-
-def get_extreme_points(col: list | str, midpoint: int, remove_range: int, right_remove_range: int = None) -> tuple[int, int]:
-    left_range = remove_range
-    right_range = right_remove_range if right_remove_range is not None else remove_range
-    min_point = max(midpoint-left_range, 0)
-    max_point = min(midpoint+right_range+1, len(col))
-    return min_point, max_point
-
-
-def to_list(smth) -> list:
-    return [smth] if isinstance(smth, str) else list(smth)
+@curry
+def map_n(func: Callable, iterable: Iterable, n: int | tuple[int]):
+    to_change = to_iter(n)
+    return (func(elem) if i in to_change else elem for i, elem in enumerate(iterable))
 
 
 @curry
-def is_instance_of(reduct_func, atype, iterable: Iterable):
-    return reduct_func(map(lambda elem: isinstance(elem, atype), iterable))
+def map_nth(n: int, iterable: Iterable):
+    return map(nth(n), iterable)
+
+
+def map_arg(*funcs_or_num_funcs, **pos_funcs):
+    def handle_positionals(args: list) -> list:
+        '''
+        Assumes "funcs_or_num_funcs" in form (1, F1, 3, F2, ...) or (F1, F2, None, F4, ...)
+        '''
+        prev_i = None
+        for i, func in enumerate(funcs_or_num_funcs):
+            i = prev_i or i
+            prev_i = func if is_int(func) else None
+            if not is_int(func):
+                args[i] = func(args[i]) if func else args[i]
+        return args
+
+    def handle_dicts(args: list) -> list:
+        '''
+        Assumes "pos_funcs" in form: {_0=F1, _1=F2, __1, ...} (not necessarily in order)
+        where _ means positive and __ means negative
+        '''
+        for _i, func in pos_funcs.items():
+            sign = 2*int(not _i.startswith('__')) - 1
+            i = sign * int(_i.removeprefix('_').removeprefix('_'))
+            args[i] = func(args[i])
+        return args
+
+    def decorator(orig_func):
+        def wrapper(*args, **kwargs):
+            map_args = fjoin(list, handle_positionals, handle_dicts, tuple)
+            new_args = map_args(args)
+            return orig_func(*new_args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def to_iter(smth) -> Iterable:
+    return smth if isinstance(smth, Iterable) and not isinstance(smth, str) else (smth, )
+
+
+def to_list(smth) -> list:
+    return [smth] if isinstance(smth, str | int | float | bool | None) else list(smth)
+
+
+@curry
+def is_instance_of(reduct_func: Callable, _type: Type, iterable: Iterable):
+    return reduct_func(map(lambda elem: isinstance(elem, _type), iterable))
 
 
 is_all_instance_of = is_instance_of(all)
@@ -113,9 +127,36 @@ def is_all_len_n(n: int, iterable: Iterable):
 is_all_len_one = is_all_len_n(1)
 
 
-def is_list_of_dicts(to_check: list) -> bool:
+def is_list_of_simple_dicts(to_check: list) -> bool:
     return is_all_instance_of(dict, to_check) and is_all_len_one(to_check)
 
 
 def is_list_of_basics(to_check: list) -> bool:
-    return is_all_instance_of(basic_yaml_type, to_check)
+    return is_all_instance_of(BasicYamlType, to_check)
+
+
+is_dict_of = map_arg(__1=dict.values)(is_instance_of)
+is_all_dict_of = is_dict_of(all)
+is_any_dict_of = is_dict_of(any)
+is_all_dict_of_none = is_all_dict_of(None)
+is_any_dict_of_none = is_any_dict_of(None)
+
+
+def map_conf_list_to_dict(to_map: List[AnyStr | Any] | Dict[AnyStr, Any]) -> Dict[AnyStr, Any]:
+    match to_map:
+        case d if is_dict(d) and is_all_instance_of(AnyStr, d.keys()):
+            return to_map
+        case l if is_list(l) and is_all_instance_of(AnyStr, l):
+            return {elem: None for elem in to_map}
+        case l if is_list(l) and is_all_instance_of(Dict, l):  # TODO: should form as default identifier be used?
+            return {elem['form']: elem for elem in to_map}
+        case _:
+            raise NotImplementedError
+
+
+def apply_to_tree(elems: list[T], apply_func: Callable, get_children: Callable[[T], Iterable[T]] = lambda curr: curr.children) -> None:
+    to_applies = elems[:]
+    while to_applies:
+        curr: T = to_applies.pop()
+        apply_func(curr)
+        to_applies.extend(get_children(curr))
