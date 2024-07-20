809ed9bdd4b734d81f079869eed43ee64ab36444
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-07-20 19:57:42 +0200

    refactor tests a bit

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -7,16 +7,34 @@ from typing import Iterable
 
 from abstractTest import AbstractTest
 from unittest.case import SkipTest
+import pydash as _
 
 all_tests = []
 
 
-def get_tests_from_dir(dir_name: str = None, name_pattern: str = None):
+def get_files_at_nth_level(dir: Path, branching_level):
+    to_enter = list(dir.iterdir())
+    while branching_level > 1:
+        curr_to_enter = to_enter[:]
+        to_enter = []
+        for dir in curr_to_enter:
+            to_enter.extend(dir.iterdir())
+        branching_level -= 1
+    return to_enter
+
+
+def get_tests_from_dir(dir_name: str = None, name_pattern: str = None, *, branching_level=1):
     if not dir_name:
         return all_tests
+
     test_dir_path = Path(__file__).parent / dir_name
-    for path in test_dir_path.iterdir():
-        module = importlib.import_module(f'{dir_name}.{path.stem}')
+    test_files = get_files_at_nth_level(test_dir_path, branching_level)
+    test_files = [file for file in test_files if '__pycache__' not in file.name]
+    for file in test_files:
+        module_path_parts = file.parts[-(branching_level+1):]
+        module_path = '.'.join(module_path_parts).removesuffix('.py')
+        module = importlib.import_module(module_path)
+
         for name, test_class in inspect.getmembers(module, inspect.isclass):
             if name.endswith('Test'):
                 if name_pattern is None or re.match(name_pattern, name):
@@ -34,7 +52,7 @@ def run_tests(to_runs: Iterable):
             errors += test.errors
             total += test.total
         else:
-            skipped += 1  #test.skipped
+            skipped += 1  # test.skipped
 
     print()
     print('#' * (2 * AbstractTest.half_sep_length))
@@ -43,4 +61,8 @@ def run_tests(to_runs: Iterable):
 
 
 if __name__ == '__main__':
-    run_tests(get_tests_from_dir('test_cases'))
+    tests = _.flatten([
+        get_tests_from_dir('feature_tests', branching_level=2),
+        get_tests_from_dir('test_cases', branching_level=1),
+    ])
+    run_tests(tests)
