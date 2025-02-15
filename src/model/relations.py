a616980e61fdc5842e2d6621c036f7cad3064f50
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-02-15 21:08:27 +0100

    init rels

---
+++ src/model/relations.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+
+from src.model.constants import RelNames as R
+
+
+@dataclass
+class RelConf:
+    name: str
+    from_label: str = field(default='')
+    to_label: str = field(default='')
+
+
+rel_conf = [
+    RelConf(R.EX),  # Hyper/Hypo(nymy)
+    RelConf(R.FORMS),
+    RelConf(R.YIELDS),
+    RelConf(R.THROUGH),
+]
