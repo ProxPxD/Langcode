5c65b2942d232fa54121a41dcc6ec9c02ab49b1e
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-02-16 17:52:04 +0100

    rel Names

--- src/model/constants.py
+++ src/model/constants.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class RelNames:
     EX = 'EX'
-    FORMS = 'FORMS'
-    YIELDS = 'YIELDS'
+    IS = 'IS'
+    FORMS = 'FORMS'  # ?
+    YIELDS = 'YIELDS'  # ?
     THROUGH = 'THROUGH'  # Same as 'EX' ?
 
