598d40a60309383baf30a4dc10d85818c0348fe9
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-07-27 16:19:32 +0200

    loading

--- src/loading/file_loaders.py
+++ src/loading/file_loaders.py
@@ -2,10 +2,12 @@ import json
 
 import yaml
 
-from loaders import FileLoader
+from loaders import FileLoader, DirLoader
 
 default_loaders = [
     YamlLoader := FileLoader(yaml.safe_load, ('.yaml', '.yml')),
     JsonLoader := FileLoader(json.load, '.json'),
     # TODO: toml loader
 ]
+
+GenLoader = DirLoader(*default_loaders)
