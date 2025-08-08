01afccdc8055f70fd3856ac71203ca8eeba01019
ProxPxD <piotr10tutek@poczta.onet.pl>
2025-08-08 21:23:29 +0200

    fix and extend LoginData

--- src/logic/constants.py
+++ src/logic/constants.py
@@ -1,7 +1,19 @@
+from src.logic.gdm import LoginData
 
-URI = 'bolt://localhost:7687'
-USER = 'neo4j'
-PASSWORD = 'password'
-DB = 'neo4j'
+neo4j_log = LoginData(
+    uri='bolt://localhost:7687',
+    user='neo4j',
+    password='password',
+    database='neo4j',
+)
+
+rdf_log = LoginData(
+    uri='http://localhost:7200',
+    user='langcode',
+    password='langcode',
+    database='langcode-dev',
+)
+
+log = rdf_log
 
 TIMEOUT = 5
--- src/logic/gdm/login_data.py
+++ src/logic/gdm/login_data.py
@@ -1,14 +1,21 @@
-from typing import Optional, Any
+from __future__ import annotations
+
+import re
+from typing import Any
 
 from pydantic import BaseModel, model_validator
 
 
+uri_pattern: re.Pattern = re.compile(r'(?P<protocol>[^:]+)://(?P<host>[^:]+):(?P<port>\d{1,5})')
+
+
 class LoginData(BaseModel):
-    port: int = None
+    protocol: str = None
     host: str = None
+    port: int = None
     user: str = None
     password: str = None
-    database: Optional[str] = None
+    database: str = None
 
     @property
     def auth(self) -> tuple[str, str]:
@@ -16,39 +23,38 @@ class LoginData(BaseModel):
 
     @property
     def uri(self) -> str:
-        return f'{self.host}:{self.port}'
+        return f'{self.protocol}://{self.host}:{self.port}'
 
     @model_validator(mode='before')
-    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
-        data = self.validate_auth(**data)
-        data = self.validate_uri(**data)
-        data = self.adjust_names(**data)
+    @classmethod
+    def validate(cls, data: dict[str, Any]) -> dict[str, Any]:
+        data = cls._validate_auth(**data)
+        data = cls._validate_uri(**data)
+        data = cls._adjust_names(**data)
         return data
 
     @classmethod
-    def validate_auth(cls,
-            auth: tuple[str, str] | tuple[str] = None,
-            user: str = None,
-            password: str = None,
-            **data
-        ) -> dict:
+    def _validate_auth(cls,
+                       auth: tuple[str, str] | str = None,
+                       user: str = None,
+                       password: str = None,
+                       **data
+                       ) -> dict:
         if not (auth or user and password):
             raise ValueError(f'Logging requires "auth" or "user" and "password" in init')
-        data.update(
-            user=user or auth[0],
-            password=password or auth[-1],
-        )
+        user, password = (user, password) if user and password else (auth.split(':') if isinstance(auth, str) else auth)
+        data.update(user=user, password=password)
         return data
 
     @classmethod
-    def validate_uri(cls, uri: str, host: str, port: int | str, **data) -> dict:
-        if not (uri or host and port):
+    def _validate_uri(cls, uri: str = None, protocol: str = None, host: str = None, port: int | str = None, **data) -> dict:
+        if not (uri or protocol and host and port):
             raise ValueError(f'Logging requires "uri" or "host" and "port" in init')
-        host, port = (host, port) if host and port else uri.split(':')
-        data.update(host=host, port=int(port))
+        protocol, host, port = (protocol, host, port) if protocol and host and port else uri_pattern.match(uri).groups()
+        data.update(protocol=protocol, host=host, port=int(port))
         return data
 
     @classmethod
-    def adjust_names(cls, database: str = None, db: str = None, **data) -> dict:
+    def _adjust_names(cls, database: str = None, db: str = None, **data) -> dict:
         data.update(database=database or db)
         return data
